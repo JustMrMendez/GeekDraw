860f54f6f74daa5606200a388d4d7b1cd7c7d068
Mr. Mendez <mrmendez@amvestor.com>
2022-10-13 00:43:44 +0000

    init

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+import cards from "./cards.json";
+
+for (const [suit, value] of Object.entries(cards)) {
+  console.log(suit, value);
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(cards);
 };
